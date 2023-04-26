c9d5a9d23b199b6da6aa0f32e3e34718bfb1182b
Joshua Balladares <joshua.balladares@urios.edu.ph>
2023-04-26 06:08:27 +0000

    push updates

--- sample.c
+++ sample.c
@@ -6,7 +6,12 @@ int main(){
 	printf("Hello, I'm Karl Bejerano");
 	printf("Hello, I'm Gail Villahermosa");
 	printf("Hello, I'm Drew Shein");
+<<<<<<< Updated upstream
 	printf("Hi from branch staging -edited");
+=======
+	printf("Hi from branch staging");
+	printf("add new features");
+>>>>>>> Stashed changes
 	return0;
 }
 
